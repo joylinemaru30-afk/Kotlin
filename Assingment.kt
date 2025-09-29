d7cc37d6d6fdd4c925336c3bcec3469a59869cf8
pi342us-g <ndubipius96@gmail.com>
2025-09-29 12:18:38 +0300

    updated code

---
+++ Assingment.kt
@@ -0,0 +1,17 @@
+fun main(){
+    //multiplicatoin
+    val  number1:Int=25
+    val number2:Int=15
+
+    //variable for product
+    val product:Int=number1*number2
+    val Divide:Int=number1/number2
+
+    //the output
+    println("The product of the number is $product")
+    println("The total is:$Divide")
+
+
+
+
+}
---
+++ Lesson1c.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //addition program
+    val number1:Int=50
+    val number2:Int=65
+
+    //create a variable for total
+    val total: Int=number1+number2
+
+    //show output
+    println("the sum of the number is :$total")
+
+} 
