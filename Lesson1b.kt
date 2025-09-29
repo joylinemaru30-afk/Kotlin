0e58cb7d4ec2ebe3061cf8f64e90c510d822776b
pi342us-g <ndubipius96@gmail.com>
2025-09-29 11:57:00 +0300

    updated code

---
+++ Lesson1b.kt
@@ -0,0 +1,16 @@
+fun main(){
+    // we shall be specifying the data types of all the variables 
+    val number1:Int=5
+    val number2:Double=60.23
+    val letter:Char='j'
+    val paidloan:Boolean=false
+    val name:String="James"
+
+    //print  the contents of the variables
+    println(number1)
+    println(number2)
+    println(letter)
+    println(paidloan)
+    println(name)
+
+}
