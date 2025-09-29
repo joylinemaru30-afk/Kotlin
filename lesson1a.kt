afaae72eacddc724cf632a0812974fa6e1142401
pi342us-g <ndubipius96@gmail.com>
2025-09-29 11:37:29 +0300

    updated code

---
+++ lesson1a.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    //This is a comment
+    // A variable is  a container that holds a value
+    //in kotlin we have so many types of initializers i.e var & val
+    //var its used in values that keeps changing/muttable
+    //valits for values that are constant/immuttable
+
+    val name="Beingjoy"//string~it is a series of characters
+    val number1=5//intrger data type(int)
+    val number2=7.85//double~a number with a decimal
+    val letter="j"//char~character(it is a single letter of the alphabet)
+    val isRegistered=true//boolean~it evaluates to either true or false
+
+    print(name)
+    print(number1)
+    print(number2)
+    print(letter)
+    print(isRegistered)
+}
